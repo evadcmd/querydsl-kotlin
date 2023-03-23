c074888fdfc51ebbc1c488dd918d8d781e6cb05b
邱敏達 <evadcmd@kyuubidatsus-MacBook-Pro.local>
2023-03-24 02:13:10 +0900

    add testcases

--- src/test/kotlin/com/gmail/evadcmd/dslkotlin/JPATests.kt
+++ src/test/kotlin/com/gmail/evadcmd/dslkotlin/JPATests.kt
@@ -1,23 +1,33 @@
 package com.gmail.evadcmd.dslkotlin
 
 import com.gmail.evadcmd.dslkotlin.entity.Event
+import com.gmail.evadcmd.dslkotlin.entity.Venue
 import com.gmail.evadcmd.dslkotlin.repo.EventRepo
 import com.gmail.evadcmd.dslkotlin.repo.VenueRepo
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-/*
 @SpringBootTest
-class JPATests(
-    private val eventRepo: EventRepo,
-    private val venueRepo: VenueRepo,
-){
+class JPATests{
+    @Autowired
+    lateinit var eventRepo: EventRepo
+    @Autowired
+    lateinit var venueRepo: VenueRepo
+
     @Test
-    fun jpaFetchTest() {
+    fun jpaFetchTest1() {
         val events: List<Event> = eventRepo.findAll()
         for (event in events) {
             println(event.name)
         }
     }
+
+    @Test
+    fun jpaFetchTest2() {
+        val venues: List<Venue> = venueRepo.findAll()
+        for (venue in venues) {
+            println(venue.addr)
+        }
+    }
 }
-*/
---
+++ src/test/kotlin/com/gmail/evadcmd/dslkotlin/QueryDSLTests.kt
@@ -0,0 +1,24 @@
+package com.gmail.evadcmd.dslkotlin
+
+import com.gmail.evadcmd.dslkotlin.entity.QEvent.event
+import com.gmail.evadcmd.dslkotlin.entity.QVenue.venue
+import com.gmail.evadcmd.dslkotlin.entity.Venue
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QueryDSLTests {
+    @Autowired
+    lateinit var query: JPAQueryFactory
+
+    @Test
+    fun queryDSLTest1() {
+        // n + 1 problem
+        val venues: List<Venue> = query.selectFrom(venue).join(event).where(event.id.`in`(52L)).fetch()
+        for (venue in venues) {
+            println(venue.addr)
+        }
+    }
+}
